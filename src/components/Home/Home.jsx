104f2dc87dc5a24a139bf9ac0da5c90e1e81b03f
Akash Khan <akashcc48@gmail.com git config --global user.email akashcc48@gmail.com git config --global user.email akashcc48@gmail.com>
2024-11-26 21:31:40 +0600

    my another portfolio added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import SocialAcount from "../SocialAccount/SocialAcount";
 import DownloadButton from "../DownloadButton";
 import TypeAnimationCom from "../TypeAnimationCom";
 import BioText from "../BioText/BioText";
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -15,6 +16,13 @@ const Home = () => {
         </h2>
         <div className="text-center flex flex-col justify-center text-black dark:text-gray-400 text-sm md:text-xl">
           <BioText />
+          <Link
+            href="https://akash-khan-2.vercel.app/"
+            target="_blank"
+            className="text-[#F31559] underline underline-offset-4 my-5"
+          >
+            Visit My Another Portfolio
+          </Link>
         </div>
         <SocialAcount />
 
